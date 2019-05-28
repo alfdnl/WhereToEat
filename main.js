ff6486298a7c4d59c05c7d8848fcd0904b88f2e4
alfdnl <alfdnl080@siswa.um.edu.my>
2019-05-28 12:55:44 +0800

    updated
    Add new list in html,
    add and delete functions in main.js
    Add CSS file

--- main.js
+++ main.js
@@ -1,52 +1,28 @@
-var listItems = document.getElementById("our-list").getElementsByTagName("li")
-var listItems2 = document.getElementById("our-list2").getElementsByTagName("li")
-var anjing
-var url = 'http://198.46.160.246:7777/api/infer'
-var clear = document.getElementById("clear")
+var url 
 var submit = document.getElementById("submit")
 
 clear.addEventListener("click", clearing);
 submit.addEventListener("click", submiting);
 
-for (var i = 0; i< listItems.length; i++) {
-	listItems[i].addEventListener("click", activateItem);
-}
 
-for (var i = 0; i< listItems2.length; i++) {	
-		listItems2[0].addEventListener("click", less);
-	 	listItems2[1].addEventListener("click", between);
-	 	listItems2[2].addEventListener("click", more);
-	 	
-}
-
-
-function activateItem(){	
-	anjing = this.innerHTML;	
-	console.log(this.innerHTML);
-	url = url + "/" +anjing;
-	console.log(url);
-}
-
-function less() {
-	url = url + "/10";
-	console.log(url)
-}
-
-function between() {
-	url = url + "/30";
-	console.log(url)
-}
-
-function more() {
-	url = url + "/55";
-	console.log(url)
-}
-
-function clearing(){
-	url = 'http://198.46.160.246:7777/api/infer'
+function myFunction() {
+  url = 'http://198.46.160.246:7777/api/infer'  
+  var customcheckbox = document.forms[0];
+  var txt = "";
+  var i;
+  for (i = 0; i < customcheckbox.length; i++) {
+    if (customcheckbox[i].checked) {
+      txt = txt + customcheckbox[i].value;
+    }
+  }
+  // document.getElementById("order").value = url +txt;
+  url = url + txt
+  console.log("The link is " + url)
+  return url;
 }
 
 function submiting(){
+	console.log("dah masuk" + url)
 	var proxyUrl = 'https://cors-anywhere.herokuapp.com/', targetUrl = url
 			fetch(proxyUrl + targetUrl)
   			.then(blob => blob.json())
